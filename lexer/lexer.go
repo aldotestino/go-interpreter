03809b12f230e9818ae4042b65f8dc832edf66cd
aldotestino <aldotestino4@gmail.com>
2024-07-18 11:31:57 +0200

    move tokens in its dedicated file

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -7,32 +7,6 @@ import (
 	"strings"
 )
 
-type TokenType string
-
-const (
-	IntTT        TokenType = "Int"
-	FloatTT      TokenType = "Float"
-	PlusTT       TokenType = "Plus"
-	MinusTT      TokenType = "Minus"
-	MultiplyTT   TokenType = "Multiply"
-	DivideTT     TokenType = "Divide"
-	OpenParenTT  TokenType = "OpenParen"
-	CloseParenTT TokenType = "CloseParen"
-	EOFTT        TokenType = "EOF"
-)
-
-type Token struct {
-	Type  TokenType
-	Value string
-}
-
-func NewToken(tokenType TokenType, value string) *Token {
-	return &Token{
-		Type:  tokenType,
-		Value: value,
-	}
-}
-
 type Lexer struct {
 	fn              string
 	text            []string
